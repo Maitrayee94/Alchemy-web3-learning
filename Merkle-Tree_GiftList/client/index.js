53cfc05b1def57ad9e5b013f506196f9498b4fa6
MD <117962426+Maitrayee94@users.noreply.github.com>
2023-01-09 11:48:58 +0530

    Add files via upload

---
+++ Merkle-Tree_GiftList/client/index.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const niceList = require('../utils/niceList.json');
+const MerkleTree = require('../utils/MerkleTree');
+
+const serverUrl = 'http://localhost:1225';
+async function main() {
+const name = process.argv[2];
+const merkleTree = new MerkleTree(niceList);
+//console.log(name);
+const index = niceList.findIndex(n => n === name);
+//console.log(index);
+
+  //const root = merkleTree.getRoot();
+  const proof = merkleTree.getProof(index);
+  //console.log("root: ", root);*/
+const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+    name,
+    proof,
+  });
+
+  console.log({ gift });
+}
+
+main();
